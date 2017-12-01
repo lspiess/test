7055866acc797f6afdb37f2c37318e1b3b678e1d
lspiess <34163731+lspiess@users.noreply.github.com>
2017-12-01 13:22:48 -0300

    Update client.js

--- client.js
+++ client.js
@@ -76,9 +76,9 @@ t.getAll();
 
 */
 
-var GLITCH_ICON = './images/glitch.svg';
-var WHITE_ICON = './images/icon-white.svg';
-var GRAY_ICON = './images/icon-gray.svg';
+var GLITCH_ICON = './glitch.svg';
+var WHITE_ICON = './icon-white.svg';
+var GRAY_ICON = './icon-gray.svg';
 
 var randomBadgeColor = function() {
   return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
